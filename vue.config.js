40ad027f095a67e785e9fe12b5e674ff7a545404
DriftKingTW <driftkingtw@gmail.com>
2021-04-18 18:20:37 +0800

    Run linter

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,31 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/vue-todo-list/'
-        : '/',
-    configureWebpack: {
-        module: {
-            rules: [
-                // ... other rules omitted
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-todo-list/" : "/",
+  configureWebpack: {
+    module: {
+      rules: [
+        // ... other rules omitted
 
-                // this will apply to both plain `.scss` files
-                // AND `<style lang="scss">` blocks in `.vue` files
-                {
-                    test: /\.sass$/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader',
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                indentedSyntax: true,
-                                // sass-loader version >= 8
-                                sassOptions: {
-                                    indentedSyntax: true
-                                }
-                            }
-                        }
-                    ]
+        // this will apply to both plain `.scss` files
+        // AND `<style lang="scss">` blocks in `.vue` files
+        {
+          test: /\.sass$/,
+          use: [
+            "vue-style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                indentedSyntax: true,
+                // sass-loader version >= 8
+                sassOptions: {
+                  indentedSyntax: true
                 }
-            ]
-        },
-    // plugin omitted
+              }
+            }
+          ]
+        }
+      ]
     }
-}
+    // plugin omitted
+  }
+};
